d6d05796362f207c816dcfad3fbf7cc54af45f0f
ajs6f <ajs6f@virginia.edu>
2013-01-25 13:07:09 -0500

    Added Fedora Record Type and simple JAXRS resource for objects/

--- src/main/java/org/fcrepo/lily/AbstractResource.java
+++ src/main/java/org/fcrepo/lily/AbstractResource.java
@@ -1,24 +1,54 @@
 package org.fcrepo.lily;
 
-import java.io.IOException;
+import javax.annotation.PostConstruct;
 
-import org.apache.zookeeper.KeeperException;
 import org.lilyproject.client.LilyClient;
-import org.lilyproject.client.NoServersException;
+import org.lilyproject.repository.api.QName;
+import org.lilyproject.repository.api.RecordType;
+import org.lilyproject.repository.api.RecordTypeNotFoundException;
 import org.lilyproject.repository.api.Repository;
 import org.lilyproject.repository.api.RepositoryException;
-import org.lilyproject.util.zookeeper.ZkConnectException;
+import org.lilyproject.repository.api.TypeManager;
+import org.lilyproject.repository.impl.valuetype.StringValueType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class AbstractResource {
 
+	static final String FNS = "fedora";
+	static final QName fedora = new QName(FNS, "fedora");
+	static final QName label = new QName(FNS, "label");
+
 	static public Repository repo = null;
 	static public LilyClient client = null;
+
+	final static private Logger logger = LoggerFactory
+			.getLogger(AbstractResource.class);
 
 	AbstractResource() {
 	}
 
 	AbstractResource(LilyClient cl) {
 		this.client = cl;
+	}
+
+	@PostConstruct
+	void initFedoraRecordType() throws RepositoryException,
+			InterruptedException {
+		logger.debug("Trying to retrieve Fedora RecordType");
+		TypeManager tm = getRepo().getTypeManager();
+		try {
+			tm.getRecordTypeByName(fedora, 1L);
+		} catch (RecordTypeNotFoundException e) {
+			logger.debug("Creating Fedora RecordType");
+			RecordType fedoraRecordType = tm.recordTypeBuilder().name(fedora)
+					.fieldEntry().defineField().name(label)
+					.type(new StringValueType()).createOrUpdate().add()
+					.createOrUpdate();
+			tm.createOrUpdateRecordType(fedoraRecordType);
+		}
+		logger.debug("Created Fedora RecordType");
+
 	}
 
 	public static Repository getRepo() {
---
+++ src/main/java/org/fcrepo/lily/FedoraObjects.java
@@ -0,0 +1,53 @@
+package org.fcrepo.lily;
+
+import java.net.URI;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import org.lilyproject.repository.api.IdGenerator;
+import org.lilyproject.repository.api.QName;
+import org.lilyproject.repository.api.Record;
+import org.lilyproject.repository.api.RecordId;
+import org.lilyproject.repository.api.Repository;
+import org.lilyproject.repository.api.RepositoryException;
+import org.lilyproject.repository.impl.id.IdGeneratorImpl;
+import org.lilyproject.repository.impl.id.UserRecordIdFactory;
+
+@Path("/objects")
+public class FedoraObjects extends AbstractResource {
+
+	IdGeneratorImpl idGenerator = (IdGeneratorImpl) client.getRepository()
+			.getIdGenerator();
+	UserRecordIdFactory userRecordIdFactory = new UserRecordIdFactory();
+
+	Repository repo = getRepo();
+
+	@Path("/{pid}")
+	@GET
+	public Response getObject(@PathParam("pid") final String pid)
+			throws RepositoryException, InterruptedException {
+		RecordId pidRecId = userRecordIdFactory.fromString(pid, idGenerator);
+
+		Record rec = repo.read(pidRecId, label);
+
+		return Response.ok((String) rec.getField(label)).build();
+	}
+
+	@Path("/{pid}")
+	@POST
+	public Response addObject(@PathParam("pid") final String pid,
+			@QueryParam("label") @DefaultValue("test") final String objLabel)
+			throws RepositoryException, InterruptedException {
+
+		repo.recordBuilder().recordType(fedora).id(pid).field(label, objLabel)
+				.createOrUpdate();
+
+		return Response.created(URI.create(pid)).build();
+	}
+}
